4d5087fc37eabe3ce4454e4108c42e21b3cebd1d
kumarvijay-msit <kumarvijay2510@gmail.com>
2017-04-18 02:34:38 +0530

    Hamburger icon added

--- app/src/main/java/com/example/vijay/parking_allocation/MainActivity.java
+++ app/src/main/java/com/example/vijay/parking_allocation/MainActivity.java
@@ -14,14 +14,14 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -96,6 +96,7 @@ public class MainActivity extends AppCompatActivity
     LatLng parkloc;
 
     Marker myMarkersrc = null, myMarkerDest = null;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,15 +105,17 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         sMapFragment = SupportMapFragment.newInstance();
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ImageView imgv = (ImageView) findViewById(R.id.imageView1);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+        imgv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+                drawer.openDrawer(Gravity.START);
+            }
+        });
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
