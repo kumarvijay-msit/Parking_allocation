183b034e984fd41977ce5e5ef5f71a0cbb56b835
Shayon <shayongupta@gmail.com>
2017-04-24 20:49:58 +0530

    added back button in two activities

--- app/src/main/java/com/example/vijay/parking_allocation/RateCard.java
+++ app/src/main/java/com/example/vijay/parking_allocation/RateCard.java
@@ -1,12 +1,23 @@
 package com.example.vijay.parking_allocation;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.vijay.parking_allocation.user.Login;
 
 public class RateCard extends AppCompatActivity {
+
+    ImageView imgv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +26,21 @@ public class RateCard extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         }
+
+        imgv = (ImageView)findViewById(R.id.imageView1);
+        imgv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                Intent it = new Intent(RateCard.this, MainActivity.class);
+                startActivity(it);
+                finish();
+
+
+            }
+        });
+
     }
 }
